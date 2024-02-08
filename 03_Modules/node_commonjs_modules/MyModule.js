9f08cd7f18c0593dcd8de41036ec467becd171fa
MikkelYtting <Mytting1996@gmail.com>
2024-02-08 09:45:00 +0100

    03 updateret + gitignore

---
+++ 03_Modules/node_commonjs_modules/MyModule.js
@@ -0,0 +1,9 @@
+class ClassA {
+    constructor(){
+        console.log("ClassA contructor");
+    }
+}
+
+module.exports = {
+    ClassA
+};
---
+++ 03_Modules/node_commonjs_modules/index.js
@@ -0,0 +1,6 @@
+const MyModule = require("./MyModule.js");
+
+const ClassA = new MyModule.ClassA();
+
+// npm -g install nodemon
+// 
