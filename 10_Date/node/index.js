2bfbafc4f45c9ed54fdcf424569b8be3e6fd3d45
MikkelYtting <Mytting1996@gmail.com>
2024-07-24 15:11:36 +0200

    push

--- 10_Date/node/index.js
+++ 10_Date/node/index.js
@@ -8,6 +8,7 @@ console.log(Date.now()) // Returnerer antallet af millisekunder siden 1. januar 
 
 
 const DateVariabel = new Date();
+console.log(DateVariabel); // Tue Jul 23 2024 14:30:00 GMT+0200 (Central European Summer Time)
 
 const DanskTidsFormat = new Intl.DateTimeFormat("da-dk").format(DateVariabel); // JS funktion til at formatere dataoer og tidspunkt
 console.log(DanskTidsFormat); // 23/07/2024"                                         
